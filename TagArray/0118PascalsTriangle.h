34812a5807d8b7a242885b8895442fc8786660f8
dhuang <daniel9huang@gmail.com>
2020-07-20 14:46:07 -0400

    initial

--- TagArray/0118PascalsTriangle.h
+++ TagArray/0118PascalsTriangle.h
@@ -6,38 +6,40 @@
 #define LEET_0118PASCALSTRIANGLE_H
 #include "../common/allHeaders.h"
 
-vector<vector<int>> generate(int numRows) {
-    vector<int> curRow, prevRow;
-    vector<vector<int>> ret;
-    int elements = 0;
-
-    for(int i = 0; i < numRows; i++) {
-        elements = i + 1;
-
-        if (i > 1) {
-            prevRow[i] = curRow[i-1];
+vector<int> calculateNextRow(vector<int> row) {
+    vector<int> nextRow;
+    int prev = 0;
+    int size = row.size();
+    for (int curr = 0; curr < size; curr++) {
+        if (curr == 0) {
+            nextRow.push_back(1);
+        } else {
+            int sum = row[prev] + row[curr];
+            nextRow.push_back(sum);
         }
+        prev = curr;
+    }
+    nextRow.push_back(1);
+    return nextRow;
+}
 
-        for(int j = 0; j < elements; j++) {
-            int prevL = 0, prevR = 0;
 
-            if (j == 0 || j == elements - 1) {
-                curRow.push_back(1);
-            } else {
-                prevL = prevRow[j-1];
-                prevR = prevRow[j];
-                curRow.push_back(prevL+prevR);
-            }
-        }
-        cout << curRow[i];
-        ret.push_back(curRow);
+vector<vector<int>> pascal(int numRows) {
+    vector<vector<int>> ret(numRows);
+    ret[0].push_back(1);
+    ret[1].push_back(1);
+    ret[1].push_back(1);
+
+    for (int i = 2; i < numRows; i++) {
+        ret[i] = calculateNextRow(ret[i-1]);
     }
     return ret;
 }
 
 void test() {
-    int x = 2;
-    generate(x);
+    int x = 5;
+    pascal(x);
+    //generate(x);
 }
 
 #endif //LEET_0118PASCALSTRIANGLE_H
